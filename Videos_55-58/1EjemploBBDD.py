7ab87d1b8dcfeb36932d73e929cd90ad2fd0788b
AnhellSteve <dantesteve@hotmail.com>
2026-05-18 08:29:41 -0500

    practica dos BBDD

--- Videos_55-58/1EjemploBBDD.py
+++ Videos_55-58/1EjemploBBDD.py
@@ -3,9 +3,31 @@ import sqlite3
 miConexion = sqlite3.connect("PrimeraBase")
 
 miCursor = miConexion.cursor()
+
 #miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+
 miCursor.execute("CREATE TABLE IF NOT EXISTS PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
-miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALON', 15, 'DEPORTES')")
+
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALON', 15, 'DEPORTES')")
+
+'''variosProductos = [
+    ("Camiseta", 10, "Deportes"),
+    ("Jarron", 90, "Ceramica"), 
+    ("Camion", 20, "Jugueteria")
+]
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES(?, ?, ?)", variosProductos)'''
+
+miCursor.execute("SELECT * FROM PRODUCTOS")
+
+variosProductos = miCursor.fetchall()
+print(variosProductos)
+
+for producto in variosProductos:
+    print(producto)
+
+for producto in variosProductos:
+    print("Nombre Articulo: ", producto[0], "- Precio: ", producto[1], "- Seccion: ", producto[2])
+
 miConexion.commit()
 
 miConexion.close()
