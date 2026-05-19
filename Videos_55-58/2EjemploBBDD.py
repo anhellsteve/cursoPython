67b4fdb9f89a35914ff257cd4efdba8e1004bed1
AnhellSteve <dantesteve@hotmail.com>
2026-05-18 20:37:48 -0500

    practica tres BBDD

---
+++ Videos_55-58/2EjemploBBDD.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+miConexion = sqlite3.connect("GestionProductos")
+miCursor = miConexion.cursor()
+
+#miCursor.execute('''CREATE TABLE IF NOT EXISTS PRODUCTOS (
+#    CODIGO_ARTICULO VARCHAR(5) PRIMARY KEY, 
+#    NOMBRE_ARTICULO VARCHAR(50), 
+#    PRECIO INTEGER, 
+#    SECCION VARCHAR(20))''')
+
+'''productos =[
+    ("AR01", "Balon", 20, "Jugueteria"),
+    ("AR02", "Pantalon", 15, "Confeccion"),
+    ("AR03", "Destornillador", 20, "Ferreteria"),
+    ("AR04", "Jarron", 45, "Ceramica")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)", productos)'''
+
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES ('AR05', 'tren', 15, 'Jugueteria')")
+
+miCursor.execute('''CREATE TABLE IF NOT EXISTS PRODUCTOS (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+    NOMBRE_ARTICULO VARCHAR(50), 
+    PRECIO INTEGER, 
+    SECCION VARCHAR(20))''')
+
+'''productos =[
+    ("Balon", 20, "Jugueteria"),
+    ("Pantalon", 15, "Confeccion"),
+    ("Destornillador", 20, "Ferreteria"),
+    ("Jarron", 45, "Ceramica")
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL,?,?,?)", productos)'''
+
+miCursor.execute("INSERT INTO PRODUCTOS VALUES (NULL, 'tren', 15, 'Jugueteria')")
+miConexion.commit()
+miConexion.close()
