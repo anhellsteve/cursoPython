f9e43775cea61c078a2194d7d3c5e3d16d9ff1f8
AnhellSteve <dantesteve@hotmail.com>
2026-05-21 20:46:55 -0500

    practica cuatro BBDD

--- Videos_55-58/2EjemploBBDD.py
+++ Videos_55-58/2EjemploBBDD.py
@@ -20,21 +20,33 @@ miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?,?)", productos)'''
 
 #miCursor.execute("INSERT INTO PRODUCTOS VALUES ('AR05', 'tren', 15, 'Jugueteria')")
 
-miCursor.execute('''CREATE TABLE IF NOT EXISTS PRODUCTOS (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
-    NOMBRE_ARTICULO VARCHAR(50), 
-    PRECIO INTEGER, 
-    SECCION VARCHAR(20))''')
+#miCursor.execute('''CREATE TABLE IF NOT EXISTS PRODUCTOS (
+#    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+#    NOMBRE_ARTICULO VARCHAR(50) UNIQUE, 
+#    PRECIO INTEGER, 
+#    SECCION VARCHAR(20))''')
 
 '''productos =[
     ("Balon", 20, "Jugueteria"),
     ("Pantalon", 15, "Confeccion"),
     ("Destornillador", 20, "Ferreteria"),
-    ("Jarron", 45, "Ceramica")
+    ("Jarron", 45, "Ceramica"),
+    ("tren", 15, "Jugueteria")
 ]
 
-miCursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL,?,?,?)", productos)'''
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (NULL,?,?,?)", productos)
 
-miCursor.execute("INSERT INTO PRODUCTOS VALUES (NULL, 'tren', 15, 'Jugueteria')")
+miCursor.execute("INSERT INTO PRODUCTOS VALUES (NULL, 'Pantalones', 35, 'Confeccion')")'''
+
+miCursor.execute("SELECT * FROM PRODUCTOS WHERE SECCION = 'Confeccion'")
+
+productos = miCursor.fetchall()
+
+print(productos)
+
+miCursor.execute("UPDATE PRODUCTOS SET PRECIO = 35 WHERE NOMBRE_ARTICULO = 'Balon'")
+
+miCursor.execute("DELETE FROM PRODUCTOS WHERE ID = 7")
+
 miConexion.commit()
 miConexion.close()
